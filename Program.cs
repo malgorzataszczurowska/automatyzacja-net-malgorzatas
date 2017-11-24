497e14fc5d84c4981b0b4d3542e7a12ecd388348
malgorzataszczurowska <33927974+malgorzataszczurowska@users.noreply.github.com>
2017-11-24 09:49:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj świecie!");
+            Console.WriteLine("Aby zamknąć aplikację wpisz EXIT i naciśnij Enter");
+            Console.ReadLine();
+            Console.Write("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+        }
+    }
+}
